de4965d07865a870f1659a3925a1cb9a112d9606
Seth Schwartz <sschwartz214@gmail.com>
2018-11-17 15:09:30 -0700

    Refactors

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,18 +1,15 @@
 class Isogram
 
   def self.isogram?(input)
-   sanitized_word = sanitize_word(input)
-   word_array = split_word(sanitized_word)
-   container = []
-
-    word_array.each do |letter|
-      if !container.include?(letter)
-        container << letter
-      else
-        return false
-      end
+    sanitized_word = sanitize_word(input)
+    word_array = split_word(sanitized_word)
+    
+    if word_array == word_array.uniq
+      return true
     end
   end
+
+  private
 
   def self.sanitize_word(word)
     no_hyphens = word.delete('-')
