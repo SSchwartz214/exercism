c17f5b42ca5a7e463f16f91133b7f962b1c48a64
Seth Schwartz <sschwartz214@gmail.com>
2019-02-24 19:52:19 -0700

    Completes DotCom class

--- java/DotComBust/src/DotComBust.java
+++ java/DotComBust/src/DotComBust.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class DotComBust {
 
-      private GameHelper helper = new GameHelper();
-      private ArrayList<DotCom> dotComList = new ArrayList<DotCom>();
-      private int numOfGuesses = 0;
+    private GameHelper helper = new GameHelper();
+    private ArrayList<DotCom> dotComList = new ArrayList<DotCom>();
+    private int numOfGuesses = 0;
 
     private void setUpGame() {
         DotCom one = new DotCom();
